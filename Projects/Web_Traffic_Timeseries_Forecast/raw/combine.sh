26f25ab8c80508c96c89d594259f8d4aa6a45511
bpJedisim <bhishantryphysics@gmail.com>
2020-10-12 22:24:04 -0400

    added web traffic data

---
+++ Projects/Web_Traffic_Timeseries_Forecast/raw/combine.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : Bhishan (Bhishan@BpMacpro.lan)
+# @file        : combine
+# @created     : Monday Oct 12, 2020 22:05:54 EDT
+#
+# @description : combine splitted binary files 
+######################################################################
+cat train_1_01 train_1_02 train_1_03 > train_1.csv.zip
+
+
