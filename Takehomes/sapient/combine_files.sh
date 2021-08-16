d98b8f3d7ba517170eebc2f87cc475efebbd59f6
bhishanpdl <bhishantryphysics@gmail.com>
2021-08-16 16:43:31 -0400

    added sapient dataset

---
+++ Takehomes/sapient/combine_files.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : Bhishan (Bhishan@BpMacpro.lan)
+# @file        : combine_files
+# @created     : Monday Aug 16, 2021 16:41:27 EDT
+#
+# @description : 
+######################################################################
+cat x* > diabetes_project_dataset.csv
+
+
